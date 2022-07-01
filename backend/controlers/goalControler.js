7feb649c18c558d06295b0a7ef7e59dd66eb03b5
imene mejri <etudiant.mejri.imene@uvt.tn>
2022-07-01 22:51:55 +0100

    initial REST API for goal

--- backend/controlers/goalControler.js
+++ backend/controlers/goalControler.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
-
+const Goal=require('../models/goalModel')
 
 //@desc Get goals
 //@route GET/api/goals
 //@access Private
 
 const getGoals = asyncHandler(async(req, res) => {
-    res.status(200).json({ msg: 'Gettt goals' });
+  const goals =await Goal.find()
+    res.status(200).json(goals);
   }) 
 //@desc post goals
 //@route POST/api/goals
@@ -15,23 +16,43 @@ const postGoals = asyncHandler ( async(req, res) => {
     if (!req.body.text){
         res.status(400)
         throw new Error('Please add a tex field ')
-
-    
     }
-  res.status(200).json({ msg: 'posttt goals' });
+    const goal = await  Goal.create({
+      text:req.body.text
+    })
+  res.status(200).json(goal);
 })
-//@desc Put goals
+
+//@desc updategoals
 //@route PUT/api/goals/:id
 //@access Private
 const putGoals =   asyncHandler( async(req, res) => {
-    res.status(200).json({ msg: 'Puttt  goals' });
+  //check if the goal is exist on the database or note
+  const goal =await Goal.findById(req.params.id)
+  if(!goal){
+    res.status(400)
+    throw new  Error('goal not found ')
+  }
+  const updatedGoal =await Goal.findByIdAndUpdate(req.params.id, req.body,{
+    new:true
+  })
+    res.status(200).json(updatedGoal);
   })
 //@desc DELETE goals
 //@route GET/api/goals/:id
 //@access Private
 const deleteGoals = asyncHandler( async (req, res) => {
-    res.status(200).json({ msg: 'DELETE goals' });
-  }) 
+  //check if the goal is exist on the database or note
+  const goal =await Goal.findById(req.params.id)
+  if(!goal){
+    res.status(400)
+    throw new  Error('goal not found ')
+  }
+   await Goal.remove()
+    res.status(200).json({msg:'the goal dleted is',id:req.params.id});
+  })
+    
+ 
 module.exports = {
   getGoals,
   putGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+
+const { erreHandler } = require('./middleware/erreurMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
-const { erreHandler}= require('./middleware/erreurMiddleware')
+connectDB();
 const app = express();
 //bodyparser
 app.use(express.json());
 app.use(express.urlencoded({ extends: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
-app.use(erreHandler)
+app.use(erreHandler);
 app.listen(port, () => console.log(`server started on ${port}`));
